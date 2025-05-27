13f97c1be8b70a674f32dd0b1f55a21a046f82da
Miguel2022104 <miguel.martinez@unac.edu.co>
2025-05-27 16:27:12 -0500

    Add feature: print app version

--- src/main/java/edu/unac/Main.java
+++ src/main/java/edu/unac/Main.java
@@ -3,5 +3,7 @@ package edu.unac;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gitflow!");
+        // Feature: print app version
+        System.out.println("Version: 1.0.0");
     }
 }
