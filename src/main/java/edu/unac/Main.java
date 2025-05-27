a9d5b6060c32e320ae3cfa940bd67a2d841897c1
Miguel2022104 <miguel.martinez@unac.edu.co>
2025-05-27 16:30:41 -0500

    Hotfix: correct developer name

--- src/main/java/edu/unac/Main.java
+++ src/main/java/edu/unac/Main.java
@@ -3,5 +3,7 @@ package edu.unac;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gitflow!");
+        // Hotfix: updated developer name
+        System.out.println("Developer: Miguel E. Martínez");
     }
 }
